a7b389a41a0c66bbaea9518e82323701c1a95dab
Dietrich Ayala <autonome@gmail.com>
2021-05-30 17:24:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
+
+import { NFTStorage, File } from 'nft.storage'
+
+/*
+const apiKey = 'YOUR_API_KEY'
+const client = new NFTStorage({ token: apiKey })
+
+const metadata = await client.store({
+  name: 'Pinpie',
+  description: 'Pin is not delicious beef!',
+  image: new File(['asdfaf'], 'pinpie.jpg', { type: 'image/jpg' })
+})
+console.log(metadata.url)
+// ipfs://bafyreib4pff766vhpbxbhjbqqnsh5emeznvujayjj4z2iu533cprgbz23m/metadata.json
+*/
